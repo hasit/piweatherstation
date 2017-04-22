5f8bd1781e30600d246a35d5930a56cdb1c6bdb7
Hasit Mistry <hasitnm@gmail.com>
2017-04-21 18:09:02 -0700

    Display weather every hour

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"strconv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 	"strings"
 	"time"
 
@@ -9,37 +12,45 @@ import (
 )
 
 func main() {
+	doEvery(1*time.Hour, getWeather)
+}
+
+func doEvery(d time.Duration, f func(time.Time)) {
+	for x := range time.Tick(d) {
+		f(x)
+	}
+}
+
+func getWeather(t time.Time) {
+	key := getAPIKey()
+	lat := "47.7579160"
+	long := "-122.2396070"
+
+	// sample request: https://api.darksky.net/forecast/22f90e2e58fc818dd33419e15933051b/47.7579160,-122.2396070?exclude=minutely,hourly,daily,alerts,flags&units=si
+	url := "https://api.darksky.net/forecast/" + key + "/" + lat + "," + long + "?exclude=minutely,hourly,daily,alerts,flags&units=si"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	w := new(Weather)
+	json.NewDecoder(resp.Body).Decode(w)
+
+	var weatherStrings []string
+
+	weatherStrings = append(weatherStrings, w.Currently.Summary)
+	tempString := fmt.Sprintf("%.6f", w.Currently.Temperature)
+	weatherStrings = append(weatherStrings, tempString)
+
 	fuse := epdfuse.NewEpdFuse()
-
-	now := time.Now()
-
-	var mmhh []string
-	mmhh = append(mmhh, strconv.Itoa(now.Hour()))
-	mmhh = append(mmhh, ":")
-	mmhh = append(mmhh, strconv.Itoa(now.Minute()))
-
-	err := fuse.WriteText(strings.Join(mmhh, " "))
+	err = fuse.Clear()
+	if err != nil {
+		panic(err)
+	}
+	err = fuse.WriteText(strings.Join(weatherStrings, "\n"))
 	if err != nil {
 		panic(err)
 	}
 }
-
-// func main() {
-// 	key := getAPIKey()
-// 	lat := "47.7579160"
-// 	long := "-122.2396070"
-
-// 	// sample request: https://api.darksky.net/forecast/22f90e2e58fc818dd33419e15933051b/47.7579160,-122.2396070?exclude=minutely,hourly,daily,alerts,flags&units=si
-// 	url := "https://api.darksky.net/forecast/" + key + "/" + lat + "," + long + "?exclude=minutely,hourly,daily,alerts,flags&units=si"
-
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer resp.Body.Close()
-
-// 	w := new(Weather)
-// 	json.NewDecoder(resp.Body).Decode(w)
-
-// 	fmt.Println(w)
-// }
